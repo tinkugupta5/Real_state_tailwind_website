27a628d38757c55e5a5696c9343f087346642b65
tinkugupta5 <tinku.gupta4@gmail.com>
2024-11-25 23:03:19 +0530

    changes in nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,13 @@ const Header = () => {
     return (
         <div className='min-h-screen bg-cover bg-center flex items-center w-full overflow-hidden' style={{ backgroundImage: "url('/header_img.png')" }} id='Header'>
             <Navbar />
-            <div className='container text-center mx-auto py-4 px-6 md:px-20 lg   '>
+            <div className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'>
             {/* mx-auto py-4 px-6 md:px-20 lg:px-32 text-white */}
-                <h2>Explore Homes that fit your dreams</h2>
+                <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20'>Explore Homes that fit your dreams</h2>
                 <div>
                     <a href='#'>Projects</a>
                     <a href='#'>Contact Us</a>
-                    <a href='#'>Contact Us</a>
+                    <a href='#'>About Us</a>
                 </div>
             </div>
         </div>
