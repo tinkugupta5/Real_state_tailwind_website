1b2dfa4ca49dac0dacdd21cf31bdff6909623e8c
tinkugupta5 <tinku.gupta4@gmail.com>
2024-12-05 22:59:35 +0530

    chnages in the blocks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
                                 Erin Lindford
                             </p>
                             <p class="text-slate-500 font-medium">
-                                Product Engineer
+                                Product Engineers
                             </p>
                         </div>
                         <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
