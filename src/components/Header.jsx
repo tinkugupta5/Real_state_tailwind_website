28795b3c27f08b4d63a6ec3df22eceb704215ba2
tinkugupta5 <tinku.gupta4@gmail.com>
2024-12-10 23:14:15 +0530

    chnages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
             <Navbar />
             <div className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'>
                 {/* mx-auto py-4 px-6 md:px-20 lg:px-32 text-white */}
-                <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20'>Explore Homes that fit your dreams</h2>                   
+                <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20'>Explore Homes That Fit Your Dreams</h2>                   
             </div>
         </div>
     )
