723eb996f720859bce60a0c0399582e63daad7d6
tinkugupta5 <tinku.gupta4@gmail.com>
2024-12-14 23:19:11 +0530

    chnages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ const Header = () => {
     return (
         <div className='min-h-screen bg-cover bg-center flex items-center w-full overflow-hidden' style={{ backgroundImage: "url('/header_img.png')" }} id='Header'>
             <Navbar />
+            
             <div className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'>       
                 <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20'>Explore Homes That Fit Your Dreams</h2>                   
             </div>
