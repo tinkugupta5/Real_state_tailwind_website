a3e96c4d32452ff1370bc139baa93c5039472582
tinkugupta5 <tinku.gupta4@gmail.com>
2024-12-24 23:37:18 +0530

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,9 +11,8 @@ const Header = () => {
            <div className='space-x-6 mt-16'>
             <a href='#Projects' className='border border-white px-8 py-3 rounded'>Projects</a>
             <a href='#Contact' className='bg-blue-500 px-8 py-3 rounded'>Contact Us</a>
-           
+
            </div>
-          
             </div>
         </div>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,6 @@ const Navbar = () => {
 
   const [showMobileMenu,setShowMobileMenu] = useState(false);
 
-
-
   return (
     <div className='absolute top-0 left-0 w-full z-10'>
         <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent '>
@@ -15,16 +13,15 @@ const Navbar = () => {
               {/* <li> */}
                 <a href='#Header' className='cursor-pointer hover:text-gray-400'>Home</a>
                 <a href='#Header' className='cursor-pointer hover:text-gray-400'>About</a>
-                <a href='#Header' className='cursor-pointer hover:text-gray-400'>Project</a>
+                <a href='#Header' className='cursor-pointer hover:text-gray-400'>Projects</a>
                 <a href='#Header' className='cursor-pointer hover:text-gray-400'>Testimonials</a>
                 <a href='#Header' className='cursor-pointer hover:text-gray-400'>Contacts</a>
               {/* </li> */}
             </ul>
-            <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign In</button>
+            <button className='hidden md:block bg-white px-8 py-2 rounded-full'>SignIn</button>
             <img src={assets.menu_icon} className='md:hidden w-7' alt=''/>
         </div>
          {/* -------- mobile - menu ---------- */}
-
          <div className={`md:hidden ${showMobileMenu ? 'fixed w-full' : 'h-0 w-0'}  fixed w-full right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}>
            <div className='flex justify-end p-6 cursor-pointer'>
             <img src={assets.cross_icon} className='w-6 ' alt='' />
@@ -41,4 +38,4 @@ const Navbar = () => {
   
 }
 
-export default Navbar
+export default Navbar          
